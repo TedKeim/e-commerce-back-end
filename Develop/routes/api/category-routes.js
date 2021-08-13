f8264e84cd2ed97453f3f0f4a4a2b98094a5872b
Ted Keim <tedkeim@yahoo.com>
2021-08-13 13:32:57 -0400

    deleted unnecessary lines of code

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+
   Category.findAll({
     attributes: ['id', 'category_name'],
     include: [
@@ -24,7 +22,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     where: {
         id: req.params.id
@@ -63,7 +60,7 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
   Category.update(req.body, {
     where: {
         id: req.params.id
@@ -83,7 +80,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+
   Category.destroy({
     where: {
         id: req.params.id
